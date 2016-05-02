582ee6c6709cb7df47d69c8127b02afa577f8902
Sephy <nobody@noone.void>
2016-05-02 10:22:27 +0200

    Adding Connection & Configuration class
    Which extend XMPPTCPConnection & XMPPTCPConnectionConfiguration in order
    to add/handle name & isEnabled attributes.

---
+++ vMail/src/com/esgi/vMail/model/Configuration.java
@@ -0,0 +1,55 @@
+package com.esgi.vMail.model;
+
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+
+public class Configuration {
+	private XMPPTCPConnectionConfiguration configuration;
+	private String name;
+	private boolean isEnabled;
+	public Configuration(XMPPTCPConnectionConfiguration configuration, String name) {
+		this.configuration = configuration;
+		this.name = name;
+	}
+	public Configuration(XMPPTCPConnectionConfiguration configuration, String name, boolean isEnabled) {
+		this(configuration, name);
+		this.isEnabled = isEnabled;
+	}
+	public Configuration() {}
+	/**
+	 * @return the configuration
+	 */
+	public XMPPTCPConnectionConfiguration getConfiguration() {
+		return configuration;
+	}
+	/**
+	 * @param configuration the configuration to set
+	 */
+	public void setConfiguration(XMPPTCPConnectionConfiguration configuration) {
+		this.configuration = configuration;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the isEnabled
+	 */
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+	/**
+	 * @param isEnabled the isEnabled to set
+	 */
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+}
---
+++ vMail/src/com/esgi/vMail/model/Connection.java
@@ -0,0 +1,43 @@
+package com.esgi.vMail.model;
+
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+
+public class Connection extends XMPPTCPConnection {
+	private String name;
+	private boolean isEnabled;
+	public Connection(XMPPTCPConnectionConfiguration config) {
+		super(config);
+		this.name = this.getServiceName();
+	}
+	public Connection(Configuration configuration) {
+		this(configuration.getConfiguration());
+		this.name = configuration.getName();
+		this.isEnabled = configuration.isEnabled();
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the isEnabled
+	 */
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+	/**
+	 * @param isEnabled the isEnabled to set
+	 */
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+}
